212ba289040a5f7120008cd0e0a4e19a4dabfb30
VansitaGajjar <vansitaygajjar@gmail.com>
2025-07-09 11:18:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <p className="min-h-screen min-w-screen flex justify-center items-center">
+        CI/CD Pipeline with docker
+      </p>
+    </>
+  )
+}
+
+export default App
